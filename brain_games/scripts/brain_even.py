baa8d72a474211ae605517a79f5c1dae4725589f
Olha Devlysh <odevlysh@gmail.com>
2024-07-27 21:52:04 -0400

    step 5 update: added script for the game brain-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+def main():
+    import prompt
+    import random
+
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f'Hello, {name}!')
+    
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+
+    counter = 0
+    while counter < 3:
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        answer = input().lower()
+
+        if answer == "yes" and number % 2 == 0 or answer == "no" and number % 2 != 0:
+            print("Correct!")
+            counter += 1
+        else:
+            correct_answer = "yes" if number % 2 == 0 else "no"
+            print(
+                f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'."
+            )
+            print(f"Let's try again, {name} ")
+            counter = 0
+
+    print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
