524486480d54a30e241399a29fc485653b63a6ce
Olha Devlysh <odevlysh@gmail.com>
2024-08-01 21:25:40 -0400

    correct DESCRIPTION in brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -2,7 +2,7 @@
 
 import random
 
-DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no"'
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def get_question_and_answer():
