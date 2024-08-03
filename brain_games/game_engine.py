257fd394817a44badab4be8ca8e1d8ccd3d92458
Olha Devlysh <odevlysh@gmail.com>
2024-08-03 09:09:23 -0400

    Fix flake8 formatting errors

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -2,8 +2,9 @@ import prompt
 
 MAX_ROUNDS = 3
 
+
 def run_game(game):
-    print("Welcome to the Brain Games!")             
+    print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
     print(game.DESCRIPTION)
@@ -18,7 +19,10 @@ def run_game(game):
             print("Correct!")
             counter += 1
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(
+                f"'{answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+            )
             print(f"Let's try again, {name}!")
             break
 
